8f5da1dd954f99a05479b32ebe087b5590a765e1
ValentinDola <dolavalentino@gmail.com>
2026-06-24 14:47:55 +0000

    refactor: update dashboard icons and links to use Lucide icons
    - Replaced Font Awesome icons with Lucide icons for a more modern look.
    - Updated the links in the dashboard to maintain functionality while enhancing visual consistency.
    - Added new buttons for creating visas and assistance cases in the dashboard.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -11,8 +11,8 @@
                 <h3>{{ $citizenCount }}</h3>
                 <p>Registered Citizens</p>
             </div>
-            <div class="icon"><i class="fas fa-users"></i></div>
-            <a href="{{ route('citizens.index') }}" class="small-box-footer">View all <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="icon"><i width="100" height="100" data-lucide="users"></i></div>
+            <a href="{{ route('citizens.index') }}" class="small-box-footer">View all <i data-lucide="move-right"></i></a>
         </div>
     </div>
     <div class="col-lg-3 col-6">
@@ -21,8 +21,8 @@
                 <h3>{{ $visaCount }}</h3>
                 <p>Visa Records</p>
             </div>
-            <div class="icon"><i class="fas fa-passport"></i></div>
-            <a href="{{ route('visas.index') }}" class="small-box-footer">Manage visas <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="icon"><i width="100" height="100" data-lucide="id-card"></i></div>
+            <a href="{{ route('visas.index') }}" class="small-box-footer">Manage visas <i data-lucide="move-right"></i></a>
         </div>
     </div>
     <div class="col-lg-3 col-6">
@@ -31,8 +31,8 @@
                 <h3>{{ $openCaseCount }}</h3>
                 <p>Open Assistance Cases</p>
             </div>
-            <div class="icon"><i class="fas fa-hands-helping"></i></div>
-            <a href="{{ route('assistance.index') }}" class="small-box-footer">View cases <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="icon"><i width="100" height="100" data-lucide="hand-helping"></i></div>
+            <a href="{{ route('assistance.index') }}" class="small-box-footer">View cases <i data-lucide="move-right"></i></a>
         </div>
     </div>
     <div class="col-lg-3 col-6">
@@ -41,8 +41,8 @@
                 <h3>{{ $documentCount }}</h3>
                 <p>Stored Documents</p>
             </div>
-            <div class="icon"><i class="fas fa-folder-open"></i></div>
-            <a href="{{ route('documents.index') }}" class="small-box-footer">View documents <i class="fas fa-arrow-circle-right"></i></a>
+            <div class="icon"><i width="80" height="80" data-lucide="folder-open"></i></div>
+            <a href="{{ route('documents.index') }}" class="small-box-footer">View documents <i data-lucide="move-right"></i></a>
         </div>
     </div>
 </div>
@@ -55,10 +55,16 @@
             </div>
             <div class="card-body">
                 <a href="{{ route('citizens.create') }}" class="btn btn-primary mr-2 mb-2">
-                    <i class="fas fa-user-plus"></i> Register Citizen
+                    <i data-lucide="user-plus"></i> Register Citizen
                 </a>
                 <a href="{{ route('search') }}" class="btn btn-outline-primary mr-2 mb-2">
-                    <i class="fas fa-search"></i> Global Search
+                    <i data-lucide="search"></i> Global Search
+                </a>
+                <a href="{{ route('visas.create') }}" class="btn btn-outline-success mr-2 mb-2">
+                    <i data-lucide="id-card"></i> Create Visa
+                </a>
+                <a href="{{ route('assistance.create') }}" class="btn btn-outline-warning mr-2 mb-2">
+                    <i data-lucide="hand-helping"></i> Assistance Case
                 </a>
             </div>
         </div>
